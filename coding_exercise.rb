0a40a4f230c192019a5551cea182d8c15bbb82fd
Joseph Guest <joe.a.guest@gmail.com>
2014-08-24 12:44:00 +0000

    First commit with pseudo-code

---
+++ coding_exercise.rb
@@ -0,0 +1,7 @@
+#Create method to perform sorting
+#Check if job structure is a hash, return error if not
+#Loop through the hash
+#If job has dependency, add after dependent job
+#If no dependency, append to end
+
+#Test cases (same as bullet points in exercise) to build to
